966bcb8f2f34d30f947bfbd51538538d9b8562fb
aleksandre <faleqs33@gmail.com>
2025-07-24 18:18:27 +0400

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("lolka koshka");
+const house = [
+    {address: 'lolka#2', price: 3213},
+    {address: 'lolka#2', price: 3213},
+    {address: 'lolka#2', price: 3213},
+    {address: 'lolka#2', price: 3213}
+]
+
+console.log(house);
